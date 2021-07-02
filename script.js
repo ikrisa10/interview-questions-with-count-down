637c184db6c7bd7ba5b647768953b69b72117f3c
ikrisa10 <ilyakrisa@gmail.com>
2021-07-01 19:32:09 -0700

    first commit with timer

---
+++ script.js
@@ -0,0 +1,37 @@
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  
+  return {
+    total,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+const timeInMinutes = 3;
+const currentTime = Date.parse(new Date());
+const deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+initializeClock('clockdiv', deadline);
