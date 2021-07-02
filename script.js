be97df65aac57284139a264da9f206235ec63446
kpartain <karalynnleann@gmail.com>
2021-07-01 20:25:18 -0700

    Initializes body structure and starts clock

--- script.js
+++ script.js
@@ -1,37 +1,133 @@
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  
-  return {
-    total,
-    minutes,
-    seconds
-  };
+//CLOCK
+const minutesDisplay = document.querySelector("#minutes");
+const secondsDisplay = document.querySelector("#seconds");
+const timeSelect = document.querySelectorAll(".min-options");
+const startButton = document.querySelector("#start-button");
+
+function updateTimeAmount(element) {
+        minutesDisplay.innerText = element.innerText.split(":")[0]
+        secondsDisplay.innerText = element.innerText.split(":")[1];
 }
 
-function initializeClock(id, endtime) {
-  const clock = document.getElementById(id);
-  const minutesSpan = clock.querySelector('.minutes');
-  const secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+function updateMinute() {
+  console.log(minutesDisplay.innerText);
+  if(minutesDisplay.innerText == "00") {
+    return minutesDisplay.innerText == 59;
+  } else if(parseInt(minutesDisplay.innerText) <= 10) {
+    return minutesDisplay.innerText == "0" + parseInt(minutesDisplay.innerText)-1;
+  }else {
+    return minutesDisplay.innerText == parseInt(minutesDisplay.innerText)-1;
   }
+};
 
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
+function updateSecond() {
+  console.log(secondsDisplay.innerText);
+  if(secondsDisplay.innerText == "00") {
+    return secondsDisplay.innerText == 59;
+  }else if(parseInt(secondsDisplay.innerText) <= 10) {
+    return secondsDisplay.innerText == "0" + parseInt(secondsDisplay.innerText) -1;
+  } else {
+    return secondsDisplay.innerText == parseInt(secondsDisplay.innerText) -1;
+  }
 }
 
-const timeInMinutes = 3;
-const currentTime = Date.parse(new Date());
-const deadline = new Date(currentTime + timeInMinutes*60*1000);
+function runClock(){
+  setInterval(updateSecond, 1000);
+  setInterval(updateMinute, 60000);
+}
 
-initializeClock('clockdiv', deadline);
+//INTERVIEW QUESTIONS
+var timeManagement = [
+    "Tell me about a goal you set and reached and how you achieved it.",
+    "Tell me about the last time your workday ended before you were able to get everything done.",
+    "Give me an example of a time you had to prioritize certain tasks or projects over others.",
+    "Give me an example of a time when you had to handle multiple responsibilities at once. How did you prioritize your time?",
+    "Tell me about the last time you handle a long-term project. How did you keep the project on track?",
+];
+var adaptability = [
+    "Can you share about a time you had to be flexible or adaptable?",
+    "Tell me about a time when you had to be creative to solve a problem.",
+    "Tell me about a time you had to learn quickly.",
+    "Tell me about a time you made a difficult decision.",
+    "Tell me about how you work under pressure.",
+];
+var overcomingChallenges = [
+    "Tell me about a time when you handled a challenging situation.",
+    "Tell me about a time when you made a mistake. What did you do to correct it?",
+    "Give me an example of a time you made a decision that was unpopular and explain how you handled implementing it.",
+    "Tell me about a goal you failed to achieve.",
+    "Tell me about a time you felt you went above and beyond.",
+];
+var motivationValues = [
+    "Give me an example of how you set goals.",
+    "Tell me about the proudest moment in your professional career why it was meaningful to you.",
+    "Can you give me an example of a time when you felt dissatisfied with your work?",
+    "Tell me about a body of work you felt was most impactful for you or your company.",
+    "How do you stay motivated when a job requires you to perform repetitive tasks?",
+];
+var communication = [
+    "Tell me about a time when you had to say “no.”",
+    "Give me an example of a time when you persuaded someone. How’d you do it and why?",
+    "Talk about a time when you’ve had to manage up.",
+    "Tell me about a time when you had to explain a complex topic to people with less subject knowledge. How did you make sure everyone could understand you?",
+    "Tell me about a time you had to build rapport with a coworker or client whose personality was different than yours?",
+];
+var teamwork = [
+    "    Tell me about a time when you collaborated with others who were different than you.",
+    "Tell me about the best presentation you’ve given. Why was it good?",
+    "Tell me about a time when you felt like a good leader.",
+    "Can you give me an example of how you’ve contributed to the culture of previous teams, companies or groups?",
+    "Share an example of how you were able to motivate a coworker, your peers or your team.",
+];
+var conflictResolution = [
+    "Tell me about a time you disagreed with a supervisor.",
+    "Tell me about a time you had to stand up for your beliefs.",
+    "Tell me about a time when you disagreed with your manager’s leadership style or team culture.",
+    "Tell me about a time when you were in conflict with a peer and how the situation was resolved.",
+    "Tell me about a time you wish you’d handled a situation with a coworker differently.",
+];
+
+function randomInteger(min, max, incOrExc) {
+    var returnInteger = 0;
+    if (incOrExc == "inclusive") {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        returnInteger = Math.floor(Math.random() * (max - min + 1) + min);
+    } else if (incOrExc == "exclusive") {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        returnInteger = Math.floor(Math.random() * (max - min) + min);
+    }
+    return returnInteger;
+}
+
+function fillQuestions() {
+    document.getElementById("time-management").innerText =
+        timeManagement[randomInteger(0, timeManagement.length, "exclusive")];
+
+    document.getElementById("adaptability").innerText =
+        adaptability[randomInteger(0, adaptability.length, "exclusive")];
+
+    document.getElementById("overcoming-challenges").innerText =
+        overcomingChallenges[
+            randomInteger(0, overcomingChallenges.length, "exclusive")
+        ];
+
+    document.getElementById("motivation-values").innerText =
+        motivationValues[
+            randomInteger(0, motivationValues.length, "exclusive")
+        ];
+
+    document.getElementById("communication").innerHTML =
+        communication[randomInteger(0, communication.length, "exclusive")];
+
+    document.getElementById("teamwork").innerText =
+        teamwork[randomInteger(0, teamwork.length, "exclusive")];
+
+    document.getElementById("conflict-resolution").innerText =
+        conflictResolution[
+            randomInteger(0, conflictResolution.length, "exclusive")
+        ];
+}
+
+fillQuestions();
